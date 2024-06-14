dcfae3e01e8f6ab19fe58c1105780c8222cce6d6
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-06-14 13:19:21 +0600

    Update Remove Duplicates from Sorted Array.cpp

--- LeetCode/Two Pointers/Remove Duplicates from Sorted Array.cpp
+++ LeetCode/Two Pointers/Remove Duplicates from Sorted Array.cpp
@@ -1,6 +1,7 @@
 Problem: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
 
-
+Answer:
+--------
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -27,3 +28,48 @@ public:
   }
 
 };
+
+
+Full code:
+-----------
+#include <vector>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    if (nums.empty()) return 0;
+
+    int k = 1;  // Start from the first unique element
+
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
+
+int main() {
+    vector<int> nums = {1, 1, 2};
+    int expectedNums[] = {1, 2};
+    
+    int k = removeDuplicates(nums);
+    
+    assert(k == 2);
+    for (int i = 0; i < k; i++) {
+        assert(nums[i] == expectedNums[i]);
+    }
+
+    cout << "Number of unique elements: " << k << endl;
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
