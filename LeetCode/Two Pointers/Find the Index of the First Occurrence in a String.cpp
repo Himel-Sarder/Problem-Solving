08f1938c0f65726f45d7338b2391ead8829e9c29
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-06-14 13:31:39 +0600

    Update Find the Index of the First Occurrence in a String.cpp

--- LeetCode/Two Pointers/Find the Index of the First Occurrence in a String.cpp
+++ LeetCode/Two Pointers/Find the Index of the First Occurrence in a String.cpp
@@ -2,3 +2,41 @@ Problem: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in
 
 Answer:
 -------
+int strStr(string haystack, string needle) {
+    size_t pos = haystack.find(needle);
+    if (pos != string::npos) {
+        return pos;
+    } else {
+        return -1;
+    }
+}
+
+
+Full code:
+----------
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int strStr(string haystack, string needle) {
+    size_t pos = haystack.find(needle);
+    if (pos != string::npos) {
+        return pos;
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    // Test cases
+    string haystack1 = "sadbutsad";
+    string needle1 = "sad";
+    cout << "Index of first occurrence: " << strStr(haystack1, needle1) << endl; // Output: 0
+
+    string haystack2 = "leetcode";
+    string needle2 = "leeto";
+    cout << "Index of first occurrence: " << strStr(haystack2, needle2) << endl; // Output: -1
+
+    return 0;
+}
