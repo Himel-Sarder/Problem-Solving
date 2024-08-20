4df34d7d40472fcc1a90e1686865e0430326cdb8
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-08-20 22:47:21 +0600

    Create A-Make All Equal.cpp

---
+++ CodeForces/A-Make All Equal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+ 
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        vector<int> a(n);
+        map<int, int> freq;
+        int max_freq = 0;
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+            max_freq = max(max_freq, ++freq[a[i]]);
+        }
+        cout << n - max_freq << endl;
+    }
+    return 0;
+}
